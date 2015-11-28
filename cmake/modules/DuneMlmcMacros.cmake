a47250675cace9596fe3fdd888a2f3333f2583f7
Rene Milk <rene.milk@wwu.de>
2015-11-28 12:40:27 +0100

    [cmake] require fftw

--- cmake/modules/DuneMlmcMacros.cmake
+++ cmake/modules/DuneMlmcMacros.cmake
@@ -1,1 +1,7 @@
 # File for module specific CMake tests.
+find_package(FFTW REQUIRED)
+if(FFTW_FOUND)
+	include_directories(${FFTW_INCLUDES})
+	set(HAVE_RANDOM_PROBLEM 1)
+	set(COMMON_LIBS ${COMMON_LIBS} fftw3_mpi ${FFTW_LIBRARIES})
+endif(FFTW_FOUND)
